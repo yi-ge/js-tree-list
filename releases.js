e431a55cf4ce84ed02cf23128771dcc7d0bafb2d
yi-ge <a@wyr.me>
2018-01-26 14:56:36 +0800

    v1.0.4

--- releases.js
+++ releases.js
@@ -2,11 +2,9 @@ const { exec } = require('child_process')
 const package = require('./package.json')
 
 exec(
-  `git config --local user.name "yi-ge"
-    && git config --local user.email "a@wyr.me"
-    && git push --all origin
-    && git tag -a v${package.version} -m "releases v${package.version}."
-    && git push origin --tags`,
+  `git config --local user.name "yi-ge" && git config --local user.email "a@wyr.me" && git push --all origin && git tag -a v${
+    package.version
+  } -m "releases v${package.version}." && git push origin --tags`,
   (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`)
