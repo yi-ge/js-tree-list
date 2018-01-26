08b25c34d226d947dd3c1f719c09fb2a0b70df15
yi-ge <a@wyr.me>
2018-01-26 14:58:05 +0800

    update

--- releases.js
+++ releases.js
@@ -4,7 +4,9 @@ const package = require('./package.json')
 exec(
   `git config --local user.name "yi-ge" && git config --local user.email "a@wyr.me" && git push --all origin && git tag -a v${
     package.version
-  } -m "releases v${package.version}." && git push origin --tags`,
+  } -m "releases v${
+    package.version
+  }." && git push origin --tags && git fetch origin`,
   (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`)
