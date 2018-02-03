0b8aa97865c8bf267a306ea2df0314d9cd0c9bb0
yi-ge <a@wyr.me>
2018-02-04 00:20:53 +0800

    update - 2018-02-04 00:20:53

--- releases.js
+++ releases.js
@@ -4,24 +4,26 @@ const { exec } = require('child_process')
 const package = require('./package.json')
 
 let arrVersion = (package.version).split('.')
-arrVersion[2] += 1
-arrVersion = arrVersion.join('.')
-package.version = arrVersion
-const upPackage = JSON.stringify(package)
+arrVersion[2] = parseInt(arrVersion[2]) + 1
+package.version = arrVersion.join('.')
+
+const upPackage = JSON.stringify(package, null, 2)
 fs.writeFileSync(path.resolve(__dirname + '/package.json'), upPackage)
 
-// exec(
-//   `git config --local user.name "yi-ge" && git config --local user.email "a@wyr.me" && git push --all origin && git tag -a v${
-//     package.version
-//   } -m "releases v${
-//     package.version
-//   }." && git push origin --tags && git fetch origin`,
-//   (error, stdout, stderr) => {
-//     if (error) {
-//       console.error(`exec error: ${error}`)
-//       return
-//     }
-//     console.log(`stdout: ${stdout}`)
-//     console.log(`stderr: ${stderr}`)
-//   }
-// )
+exec(
+  `git add . && git commit -m "releases v${
+    package.version
+  }." && git config --local user.name "yi-ge" && git config --local user.email "a@wyr.me" && git push --all origin && git tag -a v${
+    package.version
+  } -m "releases v${
+    package.version
+  }." && git push origin --tags && git fetch origin`,
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`)
+      return
+    }
+    console.log(`stdout: ${stdout}`)
+    console.log(`stderr: ${stderr}`)
+  }
+)
