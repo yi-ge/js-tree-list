46e6b99c780fb466acfbd3898bedb71a0a4f689a
王轶 <a@wyr.me>
2018-02-03 19:45:52 +0800

    update - 2018-02-03 19:45:52

--- releases.js
+++ releases.js
@@ -1,18 +1,27 @@
+const fs = require('fs')
+const path = require('path')
 const { exec } = require('child_process')
 const package = require('./package.json')
 
-exec(
-  `git config --local user.name "yi-ge" && git config --local user.email "a@wyr.me" && git push --all origin && git tag -a v${
-    package.version
-  } -m "releases v${
-    package.version
-  }." && git push origin --tags && git fetch origin`,
-  (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`)
-      return
-    }
-    console.log(`stdout: ${stdout}`)
-    console.log(`stderr: ${stderr}`)
-  }
-)
+let arrVersion = (package.version).split('.')
+arrVersion[2] += 1
+arrVersion = arrVersion.join('.')
+package.version = arrVersion
+const upPackage = JSON.stringify(package)
+fs.writeFileSync(path.resolve(__dirname + '/package.json'), upPackage)
+
+// exec(
+//   `git config --local user.name "yi-ge" && git config --local user.email "a@wyr.me" && git push --all origin && git tag -a v${
+//     package.version
+//   } -m "releases v${
+//     package.version
+//   }." && git push origin --tags && git fetch origin`,
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       console.error(`exec error: ${error}`)
+//       return
+//     }
+//     console.log(`stdout: ${stdout}`)
+//     console.log(`stderr: ${stderr}`)
+//   }
+// )
